2514a95fd8908ce23c78e383aa5e8ce14cc85df2
FedotovEM <fedotov.eger@yandex.ru>
2022-09-23 09:12:26 +0300

    Поправки внесены

--- GAR_FIAS_WorkerService/Repository/ContextAlpha_FIAS.cs
+++ GAR_FIAS_WorkerService/Repository/ContextAlpha_FIAS.cs
@@ -59,7 +59,7 @@ namespace GAR_FIAS_WorkerService.Repository
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                string connectionString = ConfigurationHelper.GetSectionValue("ConnectionString");
+                string connectionString = ConfigurationHelper.GetSectionValue("ConnectionStringAlpha");
                 optionsBuilder.UseSqlServer(connectionString);
             }
         }
